bb3e1b922900a535a59f83a914cec8c601820ccf
Gerd Kohlberger <gerdk@src.gnome.org>
2009-04-25 19:51:50 +0200

    applets: update copyright year

--- src/pointer-capture-applet.c
+++ src/pointer-capture-applet.c
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2007-2008 Gerd Kohlberger <lowfi@chello.at>
+ * Copyright © 2007-2009 Gerd Kohlberger <lowfi@chello.at>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by

1b2113eb3c7e6098e57ca3343ee13295ef02494f
Gerd Kohlberger <gerdk@src.gnome.org>
2011-05-11 21:45:58 +0200

    Reapply: "Fix up the config.h includes"
    This reverts commit 0f08cf4c65f1a92d39069278ce5e009e78daf886.

--- src/mt-common.h
+++ src/mt-common.h
@@ -17,10 +17,6 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-#ifdef HAVE_CONFIG_H
-#include <config.h>
-#endif
-
 #ifndef __MT_COMMON_H__
 #define __MT_COMMON_H__
 
